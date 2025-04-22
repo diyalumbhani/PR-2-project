1ec6900e140b0142432aa93cc3f1d6423cb4660d
Aamir097 <aamir729039@gmail.com>
2025-04-22 14:38:36 +0530

    update

---
+++ PROG3.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	printf("* \t * \t * \t * \t * \t * \n");
+	printf("* \t D \t i \t y \t a \t * \n");
+	printf("* \t * \t * \t * \t * \t * \t");
+	getch();
+
+}
---
+++ PROG4.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	printf("* \t _ \t * \t _ \t * \t _ \t * \t _\t * \n");
+	printf("* \t _ \t D \t i \t y \t a \t a \t _ \t * \n");
+	printf("* \t _ \t * \t _ \t * \t _ \t * \t _ \t * ");
+	getch();
+
+
+}
